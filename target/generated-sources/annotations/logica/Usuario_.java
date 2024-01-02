ac55971cbd17aa82a42a54928a8b6c52d388cbb3
jj7859 <jjvasquez@neuquen.gov.ar>
2024-01-02 11:27:02 -0300

    20240102 Consultorio Odontologico

---
+++ target/generated-sources/annotations/logica/Usuario_.java
@@ -0,0 +1,16 @@
+package logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-12-19T10:30:48")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> pass;
+    public static volatile SingularAttribute<Usuario, Integer> idUsuario;
+    public static volatile SingularAttribute<Usuario, String> nombreUsuario;
+    public static volatile SingularAttribute<Usuario, String> rol;
+
+}
